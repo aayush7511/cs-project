4fc1774d293dfdbde9b00aab4761e69e6fee5698
aayush7511 <aayush7511@gmail.com>
2023-11-04 00:49:36 +0530

    "deposit money done"

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# important libraries
+import simple_colors as sc
+import datetime
+
+# connecting to sql
 import mysql.connector as m
 
 con = m.connect(username="root", password="Root@123", host="localhost")
@@ -8,28 +13,33 @@ if con.is_connected():
     print("CONNECTION ESTABLISHED")
 else:
     print("CONNECTION FAILED")
-
+# using the database
 cur.execute("use bank")
 
-start_acc_id = start_trans_id = 999
+# functions to generate sequential account and transaction numbers
+cur.execute("SELECT acc_id FROM user;")
+acc_vals = cur.fetchall()
+start_acc_id = acc_vals[-1][0]
+
+cur.execute("SELECT trans_id FROM transaction;")
+trans_vals = cur.fetchall()
+if len(trans_vals) == 0:
+    start_trans_id = 999
+else:
+    start_trans_id = trans_vals[-1][0]
 
 
 def acc_id_generator():
     global start_acc_id
-    if start_acc_id == 999:
-        return 1000
-    else:
-        start_acc_id = start_acc_id + 1
-        return start_acc_id
+    start_acc_id = start_acc_id + 1
+    return start_acc_id
 
 
 def trans_id_generator():
     global start_trans_id
-    if start_trans_id == 999:
-        return 1000
-    else:
-        start_trans_id = start_trans_id + 1
-        return start_trans_id
+
+    start_trans_id = start_trans_id + 1
+    return start_trans_id
 
 
 while True:
@@ -45,20 +55,67 @@ while True:
         aadhar_no = float(input("Enter your aadhar no: "))
         address = input("Enter the address: ")
         acc_type = input("Enter the account type: ")
+
         if acc_type == "saving":
+            # checking if savings account has sufficient balance to be created
             balance = float(input("Enter the balance [>10000]: "))
+            if balance < 10000:
+                print("Enter sufficient balance amount")
+            else:
+                cur.execute(
+                    "INSERT INTO user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
+                        acc_no, name, dob, aadhar_no, address, balance, acc_type))
+
         else:
+            # all checking accounts have a balance of 1 when created
             balance = 1
 
-        cur.execute(
-            "insert into user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
-                acc_no, name, dob, aadhar_no, address, balance, acc_type))
+            cur.execute(
+                "INSERT INTO user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
+                    acc_no, name, dob, aadhar_no, address, balance, acc_type))
 
         con.commit()
 
     # DEPOSIT MONEY
     if opt1 == 2:
-        pass
+        val = int(input("Enter your account number: "))
+        amount = float(input("Enter the amount you would like to deposit: "))
+        today = datetime.date.today()
+
+        # checking if the month is single digit
+        if today.month // 10 == 0:
+            month = "0" + str(today.month)
+        else:
+            month = str(today.month)
+
+        # checking if the day is single digit
+        if today.day // 10 == 0:
+            day = "0" + str(today.day)
+        else:
+            day = str(today.day)
+
+        date = str(today.year) + month + day
+
+        # checking if the amount is negative or zero
+        if amount <= 0:
+            print("Enter a valid amount")
+        else:
+            # getting the account numbers
+            acc_nums = []
+            for t in acc_vals:
+                acc_nums.append(t[0])
+
+            if val in acc_nums:
+                # adding deposit amount to balance
+                cur.execute("UPDATE user SET balance = balance + {0} WHERE acc_id = {1}".format(amount, val))
+
+                # adding deposit as a transaction to transaction table
+                cur.execute(
+                    "INSERT INTO transaction VALUES({0},{1},'{2}',{3},'{4}')".format(trans_id_generator(), val, date,
+                                                                                     amount, "deposit"))
+                con.commit()
+            else:
+                print(sc.red("Please enter a valid account number", ["bold"]))
 
     # WITHDRAW MONEY
     if opt1 == 3:
