37e13b0b0cd1e9139e020a76db74d4a340c7a47b
aayush7511 <aayush7511@gmail.com>
2023-12-22 17:27:25 +0530

    to print the different fonts

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
-print("hello world")
+import tkinter as tk
+from tkinter import font
 
-print("This is a new status file")
+def show_fonts():
+    root = tk.Tk()
+    root.title("Fonts in Tkinter")
 
-print("THIS IS THE BIGGEST CHANGE OF ALLL")
-print("I am adding 5 new lines of code")
+    main_frame = tk.Frame(root)
+    main_frame.pack(fill=tk.BOTH, expand=True)
 
-print("adding the last bit of change")
+    canvas = tk.Canvas(main_frame)
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-print("this is another test")
+    scrollbar = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    fonts_frame = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=fonts_frame, anchor=tk.NW)
+
+    all_fonts = list(font.families())
+
+    for font_name in all_fonts:
+        font_label = tk.Label(fonts_frame, text=font_name, font=(font_name, 12))
+        font_label.pack()
+
+    root.mainloop()
+
+show_fonts()
