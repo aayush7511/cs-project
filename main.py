ac83aaac734203930a69a2569c6f916fa26d1305
aayush7511 <aayush7511@gmail.com>
2023-12-22 15:19:04 +0530

    if else block converted to functions

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # important libraries
 import simple_colors as sc
 import datetime
-from datetime import datetime
-
+from datetime import date as dt
+from tkinter import *
 # connecting to sql
 import mysql.connector as m
 
-con = m.connect(username="root", password="Root@123", host="localhost")
+con = m.connect(user="root", password="Root@123", host="localhost", auth_plugin='mysql_native_password')
 
 cur = con.cursor()
-
 
 if con.is_connected():
     print("CONNECTION ESTABLISHED")
@@ -44,161 +43,141 @@ def trans_id_generator():
     return start_trans_id
 
 
-while True:
-    opt1 = int(input("CHOOSE FROM THE FOLLOWING OPTIONS:\n[1]Add a new account\n[2]Deposit money\n[3]Withdraw "
-                     "money\n[4]Display account info\n[5]View accounts" +
-                     "\n[6]Close accounts\n[7]Modify an account\nENTER YOUR OPTION NO: "))
-
-    # ADDING A NEW ACCOUNT
-    if opt1 == 1:
-        acc_no = acc_id_generator()
-        name = input("Enter your name: ")
-        dob = input("Enter your dob [yyyymmdd]: ")
-        aadhar_no = float(input("Enter your aadhar no: "))
-        address = input("Enter the address: ")
-        acc_type = input("Enter the account type: ")
-
-        if acc_type == "saving":
-            # checking if savings account has sufficient balance to be created
-            balance = float(input("Enter the balance [>10000]: "))
-            if balance < 10000:
-                print("Enter sufficient balance amount")
-            else:
-                cur.execute(
-                    "INSERT INTO user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
-                        acc_no, name, dob, aadhar_no, address, balance, acc_type))
-
+# adding a new account
+def add_acc(name, dob, aadhar_no, address, acc_type):
+    acc_no = acc_id_generator()
+    if acc_type == "saving":
+        # checking if savings account has sufficient balance to be created
+        balance = float(input("Enter the balance [>10000]: "))
+        if balance < 10000:
+            print("Enter sufficient balance amount")
         else:
-            # all checking accounts have a balance of 1 when created
-            balance = 1
-
             cur.execute(
                 "INSERT INTO user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
                     acc_no, name, dob, aadhar_no, address, balance, acc_type))
 
+    else:
+        # all checking accounts have a balance of 1 when created
+        balance = 1
+
+        cur.execute(
+            "INSERT INTO user VALUES( {0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(
+                acc_no, name, dob, aadhar_no, address, balance, acc_type))
+
+    con.commit()
+
+
+def dep_money(val, amount):
+    today = dt.today()
+
+    # checking if the month is single digit
+    if today.month // 10 == 0:
+        month = "0" + str(today.month)
+    else:
+        month = str(today.month)
+
+    # checking if the day is single digit
+    if today.day // 10 == 0:
+        day = "0" + str(today.day)
+    else:
+        day = str(today.day)
+
+    date = str(today.year) + month + day
+
+    # checking if the amount is negative or zero
+    if amount <= 0:
+        print("Enter a valid amount")
+    else:
+        # getting the account numbers
+        acc_nums = []
+        for t in acc_vals:
+            acc_nums.append(t[0])
+
+        if val in acc_nums:
+            # adding deposit amount to balance
+            cur.execute("UPDATE user SET balance = balance + {0} WHERE acc_id = {1}".format(amount, val))
+
+            # adding deposit as a transaction to transaction table
+            cur.execute(
+                "INSERT INTO transaction VALUES({0},{1},'{2}',{3},'{4}')".format(trans_id_generator(), val, date,
+                                                                                 amount, "deposit"))
+            con.commit()
+        else:
+            print(sc.red("Please enter a valid account number", ["bold"]))
+
+
+def withdraw_money(acc_no):
+    trans_id = trans_id_generator()
+    cur.execute("select acc_id,balance from user where acc_id = {0}".format(acc_no))
+    acc_details = cur.fetchone()
+
+    today = dt.today()
+    # checking if the month is single digit
+    if today.month // 10 == 0:
+        month = "0" + str(today.month)
+    else:
+        month = str(today.month)
+
+    # checking if the day is single digit
+    if today.day // 10 == 0:
+        day = "0" + str(today.day)
+    else:
+        day = str(today.day)
+
+    date = str(today.year) + month + day
+
+    if acc_details is None:
+        print("Account Number is not correct")
+    else:
+        withdrawal_amount = int(input("Enter amount to withdraw: "))
+        existing_balance = acc_details[1]
+        if withdrawal_amount > existing_balance:
+            print("Your withdrawal amount " + str(withdrawal_amount) + " It is more than existing balance : " + str(
+                existing_balance))
+        else:
+            new_balance = existing_balance - withdrawal_amount
+            cur.execute("UPDATE user SET balance = {0} WHERE acc_id = {1}".format(new_balance, acc_details[0]))
+            cur.execute(
+                "INSERT INTO transaction(acc_id,date,trans_amount,trans_type,trans_id) VALUES({0},'{1}',{2},'{3}',{4})".format(
+                    acc_details[0], date, withdrawal_amount, 'withdraw', trans_id))
+            print("Your current available balance" + str(new_balance))
+            con.commit()
+
+
+def disp_acc_info(acc_no):
+    cur.execute("select acc_id, name, acc_type, balance from user where acc_id = {0}".format(acc_no))
+    acc_details = cur.fetchone()
+    if acc_details is None:
+        print("Account Number is not correct")
+    else:
+        print(acc_details[0])
+        print("account number: " + str(acc_details[0]))
+        print("name: " + acc_details[1])
+        print("account type: " + acc_details[2])
+        print("balance: " + str(acc_details[3]))
+
+
+def modify(ch, acc_na1):
+    if ch == 1:
+        print("Changing Name")
+        name1 = input("Enter your new/updated name: ")
+        cur.execute("UPDATE user SET name='{0}' WHERE acc_id={1}".format(name1, acc_na1))
         con.commit()
-
-    # DEPOSIT MONEY
-    if opt1 == 2:
-        val = int(input("Enter your account number: "))
-        amount = float(input("Enter the amount you would like to deposit: "))
-        today = datetime.date.today()
-
-        # checking if the month is single digit
-        if today.month // 10 == 0:
-            month = "0" + str(today.month)
-        else:
-            month = str(today.month)
-
-        # checking if the day is single digit
-        if today.day // 10 == 0:
-            day = "0" + str(today.day)
-        else:
-            day = str(today.day)
-
-        date = str(today.year) + month + day
-
-        # checking if the amount is negative or zero
-        if amount <= 0:
-            print("Enter a valid amount")
-        else:
-            # getting the account numbers
-            acc_nums = []
-            for t in acc_vals:
-                acc_nums.append(t[0])
-
-            if val in acc_nums:
-                # adding deposit amount to balance
-                cur.execute("UPDATE user SET balance = balance + {0} WHERE acc_id = {1}".format(amount, val))
-
-                # adding deposit as a transaction to transaction table
-                cur.execute(
-                    "INSERT INTO transaction VALUES({0},{1},'{2}',{3},'{4}')".format(trans_id_generator(), val, date,
-                                                                                     amount, "deposit"))
-                con.commit()
-            else:
-                print(sc.red("Please enter a valid account number", ["bold"]))
-
-    # WITHDRAW MONEY
-    # column names used user(acc_id,name,birthday,aadhar_no,address,acc_type,balance)
-    #transaction(trans_id, acc_id,trans_date,trans_amt,trans_type)
-    if opt1 == 3:
-        acc_no = int(input("Enter your account number: "))
-        trans_id=trans_id_generator()
-        cur.execute("select acc_id,balance from user where acc_id = {0}".format(acc_no))
-        acc_details = cur.fetchone()
-        if acc_details is None:
-            print("Account Number is not correct")
-        else:
-            withdrawal_amount = int(input("Enter amount to withdraw: "))
-            existing_balance  = acc_details[1]
-            if withdrawal_amount > existing_balance:
-                print("Your withdrawal amount " + str(withdrawal_amount)  + " It is more than existing balance : " + str(existing_balance))
-            else:
-                new_balance = existing_balance - withdrawal_amount
-                cur.execute("UPDATE user SET balance = {0} WHERE acc_id = {1}".format(new_balance,  acc_details[0]))
-                cur.execute("INSERT INTO transaction(acc_id,date,trans_amount,trans_type,trans_id) VALUES({0},'{1}',{2},'{3}',{4})".format(acc_details[0], datetime.now(),withdrawal_amount,'withdraw',trans_id))
-                print("Your current available balance" + str(new_balance))
-                con.commit()
-
-    # DISPLAY ACCOUNT INFO
-    if opt1 == 4:
-        acc_no = int(input("Enter your account number: "))
-        cur.execute("select acc_id, name, acc_type, balance from user where acc_id = {0}".format(acc_no))
-        acc_details = cur.fetchone()
-        if acc_details is None:
-            print("Account Number is not correct")
-        else:
-            print(acc_details[0])
-            print("account number: "  + str(acc_details[0]))
-            print("name: " + acc_details[1] )
-            print("account type: " + acc_details[2])
-            print("balance: " + str(acc_details[3]))
+    elif ch == 2:
+        print('Changing Date of Birth')
+        dob1 = int(input('Enter new date of birth:'))
+        cur.execute('UPDATE user SET dob={0} WHERE acc_id={1}'.format(dob1, acc_na1))
+        con.commit()
+    elif ch == 3:
+        print('Changing Address')
+        add1 = input("Enter new address:")
+        cur.execute("UPDATE user SET address='{0}' WHERE acc_id={1}".format(add1, acc_na1))
+        con.commit()
+    print("Account changed !!!!! if you want to see the change go see our view accounts ")
 
 
-    # VIEW ACCOUNTS
-    if opt1 == 5:
-        pass
+# making the GUI
 
-    # CLOSE ACCOUNTS
-    if opt1 == 6:
-        print("Are you sure you want to close account?")
-        ch1=input("Enter(y/n):")
-        if ch1 in ["y","Y"]:
-            acc_na=int(input("Enter account number:"))
-            cur.execute("DELETE FROM user WHERE acc_id={}".format(acc_na))
-            con.commit()
-            print('THANK YOU')
-        elif ch1 in ["n","N"]:
-            print("THANK YOU!")
-        else:
-            print("Wrong input. Enter again")
-        print("your account has been deleted")
-
-
-    # MODIFY AN ACCOUNT
-    if opt1 == 7:
-        print('''
-        What would you wish to change:
-        1) Name
-        2) Date Of Birth
-        3) Address
-        ''')
-        ch=int(input("Enter choice(1,2,3):"))
-        acc_na1=int(input("Enter your account number:"))
-        if ch==1:
-            print("Changing Name")
-            name1 = input("Enter your new/updated name: ")
-            cur.execute("UPDATE user SET name='{0}' WHERE acc_id={1}".format(name1,acc_na1))
-            con.commit()
-        elif ch==2:
-            print('Changing Date of Birth')
-            dob1=int(input('Enter new date of birth:'))
-            cur.execute('UPDATE user SET dob={0} WHERE acc_id={1}'.format(dob1,acc_na1))
-            con.commit()
-        elif ch==3:
-            print('Changing Address')
-            add1=input("Enter new address:")
-            cur.execute("UPDATE user SET address='{0}' WHERE acc_id={1}".format(add1,acc_na1))
-            con.commit()
-        print("Account changed !!!!! if you want to see the change go see our veiw accounts ")
+# window = Tk()
+#
+# heading
